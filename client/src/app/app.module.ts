661fa2f4189f47b61f61b80b75802dd60bb2b2c3
SergioMartinAlvaro <smaral.sergio@gmail.com>
2018-03-15 00:41:15 +0100

    Created route and component to list artists in the application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { UserEditComponent } from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserEditComponent
+    UserEditComponent,
+    ArtistListComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -6,13 +6,16 @@ import { Routes, RouterModule } from '@angular/router';
 //import user
 import {UserEditComponent } from './components/user-edit.component';
 
+import {ArtistListComponent} from './components/artist-list.component';
+
 //array con todas nuestras configuraciones de rutas
 
 const appRoutes: Routes = [
 	//Para esta ruta cargamos este componente, unimos las dos partes
-	{path: '', component: UserEditComponent},
+	{path: '', component: ArtistListComponent},
+	{path: 'artist/:page', component: ArtistListComponent},
 	{path: 'mis-datos', component: UserEditComponent},
-	{path: '**', component: UserEditComponent}
+	{path: '**', component: ArtistListComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
