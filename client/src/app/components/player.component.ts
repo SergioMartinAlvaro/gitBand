190250686616d6873421c487f9ffd555fcd63483
SergioMartinAlvaro <smaral.sergio@gmail.com>
2018-04-18 00:24:35 +0200

    Creado componente del reproductor, sin vista responsive

---
+++ client/src/app/components/player.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {Song} from '../models/song';
+import {GLOBAL} from '../services/global';
+
+@Component({
+	selector: 'player',
+	template: `
+		<div class="album-image">
+			<span *ngIf="song.album">
+				<img id="play-image-album" src="{{ url + 'get-image-album/' + song.album.image}}" />
+			</span>
+			<span *ngIf="!song.album">
+				<img id="play-image-album" src="assets/img/cover/default.jpg" />
+			</span>
+		</div>
+		<div class="audio-file">
+			<p>Reproduciendo</p>
+			<span id="play-song-title">
+				{{song.name}}
+			</span>
+			|
+			<span id="play-song-artist">
+				<span *ngIf="song.artist">
+					{{song.album.artist.name}}
+				</span>
+				<audio controls id="player">
+					<source id="mp3-source" src="{{ url + 'get-song-file/' + song.file}}" type="audio/mpeg">
+					Tu navegador no es compatible con HTML5
+				</audio>
+			</span>
+		</div>
+	`
+})
+
+export class PlayerComponent implements OnInit{
+	
+	public url:string;
+	public song;
+
+	constructor(){
+		this.url = GLOBAL.url;
+		this.song = new Song(1,'','','','');
+	}
+
+	ngOnInit() {
+		console.log("El player funciona bien");
+	}
+
+}
